dee51c455f02265302394601bae203d953086155
mehkey <87057470+mehkey@users.noreply.github.com>
2023-01-19 18:51:52 -0500

    Create SpringBootGrpcApplicationTests.java

---
+++ src/test/java/com/mehkey/springbootgrpc/SpringBootGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehkey.springbootgrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
