b90245031c99ae1cec70cdc5878e1f258f7a03d5
mehkey <87057470+mehkey@users.noreply.github.com>
2023-01-19 18:52:10 -0500

    Create Client.java

---
+++ src/main/java/com/mehkey/springbootgrpc/Client.java
@@ -0,0 +1,15 @@
+package com.mehkey.springbootgrpc;
+
+import com.mehkey.springbootgrpc.HelloServiceGrpc.HelloServiceBlockingStub;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class Client {
+    public static void main(String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 6565).usePlaintext().build();
+
+        HelloServiceBlockingStub stub = HelloServiceGrpc.newBlockingStub(channel);
+        HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder().setFirstName("Meh").setLastName("Key").build());
+        System.out.println("Greeting: " + helloResponse.getGreeting());
+    }
+}
