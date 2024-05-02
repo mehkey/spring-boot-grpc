f7e40cc52005c2944bd96bfd64bff83cc95b732b
mehkey <87057470+mehkey@users.noreply.github.com>
2024-05-01 22:59:22 -0400

    Update Service.java

--- src/main/java/com/mehkey/springbootgrpc/Service.java
+++ src/main/java/com/mehkey/springbootgrpc/Service.java
@@ -3,21 +3,18 @@ package com.mehkey.springbootgrpc;
 import io.grpc.stub.StreamObserver;
 import org.lognet.springboot.grpc.GRpcService;
 
-import java.util.*;
-
 @GRpcService
-public class Service  extends HelloServiceGrpc.HelloServiceImplBase {
-    @Override
-    public void hello(HelloRequest request,
-                      StreamObserver<HelloResponse> responseObserver) {
-        String message = "Hello " + request.getFirstName() + " " + request.getLastName();
-        
-        
+public class Service extends HelloServiceGrpc.HelloServiceImplBase {
+                @Override
+                public void hello(HelloRequest request,
+                                                StreamObserver<HelloResponse> responseObserver) {
 
-        responseObserver.onNext(HelloResponse.newBuilder()
-                .setGreeting(message)
-                .build());
-        responseObserver.onCompleted();
-        System.out.println("HelloService: response message: " + message);
-    }
+                                String message = "Hello yaasdd " + request.getFirstName() + " " + request.getLastName();
+
+                                responseObserver.onNext(HelloResponse.newBuilder()
+                                                                .setGreeting(message)
+                                                                .build());
+                                responseObserver.onCompleted();
+                                System.out.println("HelloService: response message: " + message);
+                }
 }
