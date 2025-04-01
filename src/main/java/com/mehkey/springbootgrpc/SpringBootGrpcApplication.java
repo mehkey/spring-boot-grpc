dc4b85f005a9bbc8ae32c42231e636c6cd928f8d
mehkey <87057470+mehkey@users.noreply.github.com>
2025-04-01 08:49:54 -0700

    Update SpringBootGrpcApplication.java

--- src/main/java/com/mehkey/springbootgrpc/SpringBootGrpcApplication.java
+++ src/main/java/com/mehkey/springbootgrpc/SpringBootGrpcApplication.java
@@ -3,11 +3,15 @@ package com.mehkey.springbootgrpc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.Getter;
+
 @SpringBootApplication
 public class SpringBootGrpcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootGrpcApplication.class, args);
 	}
+
+	// @Getter
 
 }
